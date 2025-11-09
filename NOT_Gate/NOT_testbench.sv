14dd82d2e0a77480f20bffbd9b07ccd60502e178
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-11-09 23:37:56 +0530

    Update NOT_testbench.sv

--- NOT_Gate/NOT_testbench.sv
+++ NOT_Gate/NOT_testbench.sv
@@ -1,1 +1,21 @@
+module tb_NOT;
 
+  reg a;
+  wire y;
+
+  NOT_gate uut(a, y);
+
+  initial begin
+    // these two lines to create the waveform file
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_NOT);
+
+    // test vectors
+    a = 0;
+    #10;
+    a = 1;
+    #10;
+    $finish;
+  end
+
+endmodule
