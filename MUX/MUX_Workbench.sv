0c833b75760d83c6a8e490034b5860e0d900fa49
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-11-10 09:27:16 +0530

    Add testbench for MUX module

---
+++ MUX/MUX_Workbench.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+module tb_MUX(
+
+    );
+reg i_0, i_1, s;
+wire y;
+
+MUX uut(i_0, i_1, s, y);
+
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(0, tb_MUX);
+
+i_0 = 0;
+i_1 = 1;
+s = 0 ;
+#10
+
+i_0 = 0;
+i_1 = 1;
+s = 1 ;
+#10
+
+$finish;
+
+end
+
+endmodule
