643f0cf719bb4fa55a45fe450b8bd54ddd1e79f3
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-11-10 08:32:50 +0530

    Create testbench for OR gate functionality
    Added a testbench for the OR gate with waveform generation.

--- OR_Gate/OR_Gate_workbench.sv
+++ OR_Gate/OR_Gate_workbench.sv
@@ -1,1 +1,37 @@
+`timescale 1ns / 1ps
 
+module tb_OR(
+
+    );
+
+reg a, b;
+wire y;
+
+OR_gate uut(a, b, y);
+
+initial begin
+    // to create the waveform file
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_OR);
+
+    a = 0;
+    b = 0;
+    #10;
+
+    a = 0;
+    b = 1;
+    #10;
+
+    a = 1;
+    b = 0;
+    #10;
+
+    a = 1;
+    b = 1;
+    #10;
+
+    $finish;
+
+end
+
+endmodule
