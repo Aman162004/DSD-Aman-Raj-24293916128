e1b9ee5633a72eec3272febdbd399878bc43ad3f
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-11-19 18:16:12 +0530

    Add SR latch module in SystemVerilog

---
+++ SR_Latch/SR_Latch.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module sr_latch(
+    input S,        
+    input R,        
+    output Q,       
+    output Q_bar    
+);
+
+    assign Q = ~(R | Q_bar);
+    assign Q_bar = ~(S | Q);
+
+endmodule 
