2f1d62e16adb1e2d1dca36fdd1dd1b8db3b38cf7
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2025-12-03 22:06:53 +0530

    Update MUX_Workbench.sv

--- MUX/MUX_Workbench.sv
+++ MUX/MUX_Workbench.sv
@@ -12,15 +12,8 @@ initial begin
   $dumpfile("dump.vcd");
   $dumpvars(0, tb_MUX);
 
-i_0 = 0;
-i_1 = 1;
-s = 0 ;
-#10
-
-i_0 = 0;
-i_1 = 1;
-s = 1 ;
-#10
+i_0 = 0; i_1 = 1; s = 0 ; #10;
+i_0 = 0; i_1 = 1; s = 1 ; #10;
 
 $finish;
 
